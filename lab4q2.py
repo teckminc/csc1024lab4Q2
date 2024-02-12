6cca5a5eff5d403e9224a09919679761ab001147
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-12 15:13:12 +0800

    First commit
    First commit

---
+++ lab4q2.py
@@ -0,0 +1,26 @@
+'''
+Do not remove any text from these comments
+2.	Write a Python program using the while loop to generate a multiplication table that will have the following outputs.
+
+Which multiplication table would you like to print? 8
+How high would you like it to go? 10
+
+Here is your multiplication table:
+8 times 1 = 8
+8 times 2 = 16
+8 times 3 = 24
+8 times 4 = 32
+8 times 5 = 40
+8 times 6 = 48
+8 times 7 = 56
+8 times 8 = 64
+8 times 9 = 72
+8 times 10 = 80
+
+Functions to use: int(), input(), print(), str(), str.rjust(), str.ljust()
+Operators: <=, +=
+'''
+def main():
+
+    return 0
+
---
+++ lab4q2_test.py
@@ -0,0 +1,18 @@
+import lab4q2
+from io import StringIO
+from sys import stderr
+def test_case1(monkeypatch, capsys):
+    number_inputs = StringIO('6\n10\n')
+
+    monkeypatch.setattr('sys.stdin', number_inputs)
+    lab4q2.main()
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip().find(f'6 times 10 =  60') != -1
+    assert captured_stdout.strip().find(f'6 times 11') == -1
+
+def test_case2(monkeypatch, capsys):
+  with open(f"lab4q2.py") as tf:
+    head = [next(tf) for _ in range(22)]
+    s = tf.read()
+    assert(s.find("while") != -1 )
+
